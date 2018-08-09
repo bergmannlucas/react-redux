f736a1168beab2d75f16789a032c39909a99f987
bergmannlucas <lucaasberg@gmail.com>
2018-08-09 17:40:27 -0300

    Olá React - Exercicios React

--- exercicios_react/webpack.config.js
+++ exercicios_react/webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './ex/index.js',
+  entry: './ex.0/index.jsx',
   output: {
     path: __dirname + '/public',
     filename: './bundle.js'
