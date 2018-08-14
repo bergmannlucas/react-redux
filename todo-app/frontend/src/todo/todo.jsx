c439477f173bb27a97cdb51ca08da5f1cae37e3d
bergmannlucas <lucaasberg@gmail.com>
2018-08-14 15:21:09 -0300

    Marcar como Concluído/Pendente

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -14,6 +14,8 @@ export default class Todo extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
     this.refresh();
   }
 
@@ -36,6 +38,16 @@ export default class Todo extends Component {
     axios.delete(`${URL}/${todo._id}`)
          .then(resp => this.refresh());
   }
+
+  handleMarkAsDone(todo) {
+    axios.put(`${URL}/${todo._id}`, {...todo, done: true})
+         .then(resp => this.refresh());
+  }
+
+  handleMarkAsPending(todo) {
+    axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+         .then(resp => this.refresh());
+  }
   
   render() {
     return (
@@ -45,7 +57,9 @@ export default class Todo extends Component {
             handleChange={this.handleChange} 
             handleAdd={this.handleAdd}/>
         <TodoList list={this.state.list}
-            handleRemove={this.handleRemove}/>
+            handleRemove={this.handleRemove}
+            handleMarkAsDone={this.handleMarkAsDone}
+            handleMarkAsPending={this.handleMarkAsPending}/>
       </div>
     );
   }
