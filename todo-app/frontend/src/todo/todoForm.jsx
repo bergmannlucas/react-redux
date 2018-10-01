c089e94524350501ed742c11f32b838a16102eb3
bergmannlucas <lucaasberg@gmail.com>
2018-10-01 12:37:47 -0300

    Melhorando a Action de Pesquisa

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -42,7 +42,7 @@ class TodoForm extends Component {
               onClick={() => add(description)}>
           </IconButton>
           <IconButton style='info' icon='search'
-              onClick={() => search()}>
+              onClick={search}>
           </IconButton>
           <IconButton style='default' icon='close'
               onClick={() => clear()}>
