d5050ace555773863fde73a19cd634303e2a2678
bergmannlucas <lucaasberg@gmail.com>
2018-10-16 15:58:18 -0300

    Componente  Rodapé

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2018
+      <a href='http://github.com/bergmannlucas' target='_blank'> Bergmann</a>.
+    </strong>
+  </footer>
+);
